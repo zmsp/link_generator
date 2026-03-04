1b1881dd18e824a4ae742f3cb3477e05e3f2cebf
zmsp <zmsp@users.noreply.github.com>
2026-03-04 08:26:48 +0000

    Deploy from zmsp/link_generator@99972ccacef49e54862a28de7d2c5b9c2bf51bb1

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"327ed814500baca0480a02f8555aa84a9afe32
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1814630456" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1593474855" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
